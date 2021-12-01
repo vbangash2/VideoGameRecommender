789bbf8cbd0b5598cb5ab8e84931920db9372abf
vbangash2 <69874717+vbangash2@users.noreply.github.com>
2021-12-01 12:44:44 -0500

    Update classificationAlg.py

--- BetaRecommender/classificationAlg.py
+++ BetaRecommender/classificationAlg.py
@@ -1,6 +1,148 @@
-# implement classification alg
-# KNN classifier
+## implement classification alg
 
-# Get user name and ratings
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# pull data from X amount of games
+from google.colab import files
+
+uploaded = files.upload()
+
+for fn in uploaded.keys():
+  print('User uploaded file "{name}" with length {length} bytes'.format(
+      name=fn, length=len(uploaded[fn])))
+  
+ratings = pd.read_csv('ratings.csv')
+ratings.head()
+unique_user = ratings.userId.nunique(dropna = True)
+unique_movie = ratings.movieId.nunique(dropna = True)
+print("number of unique user:")
+print(unique_user)
+print("number of unique movies:")
+print(unique_movie)
+# for creating item user matrix  .. we need to check how many ratings we have here or how many are absent .
+total_ratings = unique_user*unique_movie
+rating_present = ratings.shape[0]
+
+ratings_not_provided = total_ratings - rating_present 
+
+print("ratings not provided means some user have not watched some movies and its given by")
+print(ratings_not_provided)
+print("sparsity of user-item matrix is :")
+print(ratings_not_provided / total_ratings)
+# 1)plot ratings count which gives information about which rating(on scale of 0 t0 5) is more frequent
+
+rating_cnt = pd.DataFrame(ratings.groupby('rating').size(),columns=['count'])
+rating_cnt
+# this rating cnt doesnt contain count of rating 0
+# append rating_cnt
+
+rating_cnt = rating_cnt.append(pd.DataFrame({'count':ratings_not_provided},index = [0])).sort_index()
+rating_cnt
+
+#since the count of rating 0 is too large in comparison to others rating ...use lag value
+rating_cnt['log_count'] = np.log(rating_cnt['count'])
+rating_cnt
+
+# plot a bar plot to visualise the ratings
+rating_cnt_for_vis = rating_cnt
+ax = rating_cnt_for_vis.reset_index().rename(columns = {'index':'rating_value'}).plot(
+    x='rating_value',
+    y='count',
+    logy = True,
+    kind='bar',
+    title='count for each rating in log scale',
+    figsize=(12,6)
+)
+ax.set_xlabel('rating_value')
+ax.set_ylabel('count of each rating')
+print("frequency of rating like 3 and 4 are more in compare to other ratings")
+
+# 2) plot rating frequency of each movie(how many time a movie has been rated)
+
+movie_freq = pd.DataFrame(ratings.groupby('movieId').size(),columns=['count'])
+movie_freq.head()
+# plot movie rating freq
+movie_freq_copy = movie_freq.sort_values(by='count',ascending=False)
+movie_freq_copy=movie_freq_copy.reset_index(drop=True)
+
+ax1  = movie_freq_copy.plot(
+    title='rating frquency of movie',
+    logy=True,
+    figsize=(12,8)
+)
+ax1.set_xlabel('number of movies')
+ax1.set_ylabel('rating freq of movies')
+
+# we can see that most of the movies are rated very rarely ..
+# so we can remove those movies which are rated less than 50 times.
+
+threshold_rating_freq = 10
+# first take out the movie id  for which movie is rated more than threshold value than keep only this movies in our original ratings dataframe
+# movie_freq.query('count>= @threshold_rating_freq').shape = (13360,1)
+# our original movie_freq has shape of (9724 ,1) and now its reduce to (2269,1)
+# so now lets reduce the size of ratings dataframe
+
+popular_movies_id = list(set(movie_freq.query('count>=@threshold_rating_freq').index))
+
+# ratings df after dropping non popular movies
+ratings_with_popular_movies = ratings[ratings.movieId.isin(popular_movies_id)]
+
+print('shape of ratings:')
+print(ratings.shape)
+
+print('shape of ratings_with_popular_movies:')
+print(ratings_with_popular_movies.shape)
+
+print("no of movies which are rated more than 50 times:")
+print(len(popular_movies_id))
+
+
+print("no of unique movies present in dataset:")
+
+user_cnt = pd.DataFrame(ratings.groupby('userId').size(),columns=['count'])
+user_cnt_copy = user_cnt
+user_cnt.head()
+
+# plot rating freq
+ax  = user_cnt_copy.sort_values('count',ascending=False).reset_index(drop=True).plot(
+      title='rating freq by user',
+      figsize=(12,6),
+      
+)
+ax.set_xlabel("users")
+ax.set_ylabel("rating frequency")
+
+# you cans see tha rating frequency vs users characterstics is tail - like structure which is similar to previous plot.
+# generally there are just few user who are interseted in giving rating to movies
+# lets find the user who gives rating more than 30 times
+threshold_val = 30
+active_user = list(set(user_cnt.query('count>=@threshold_val').index))
+
+#upadte your ratings_with_popular_movies
+ratings_with_popular_movies_with_active_user = ratings_with_popular_movies[ratings_with_popular_movies.userId.isin(active_user)]
+
+print('shape of ratings_with_popular_movies:')
+print(ratings_with_popular_movies.shape)
+
+print('shape of ratings_with_popular_movies_with_active_user:')
+print(ratings_with_popular_movies_with_active_user.shape)
+
+print("unique_user:")
+print(unique_user)
+
+print("active_user")
+print(len(active_user))
+
+print("unique_movies")
+print(unique_movie)
+
+print("popular_movies")
+print(len(popular_movies_id))
+
+print("sparsity of final ratings df:")
+print( (428*2269 - 76395)/(428*2269) )
+
+final_ratings = ratings_with_popular_movies_with_active_user
+#final_ratings.shape
+item_user_mat = final_ratings.pivot(index='movieId',columns = 'userId',values='rating').fillna(0)
